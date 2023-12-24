7ca2516116bf2c600a5c411141afd19ec115f9da
Lhafer <shaferlogancfl@gmail.com>
2023-12-24 03:13:50 -0500

    Under Banner Section
    Added Tech Stack Table under the banner.
    Banner now scrolls back down with the "Show Less" button instead of the about me button at the top of the screen.

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ window.addEventListener("scroll", function () {
 
 function scrollBanner(element) {
   var banner = document.getElementById("about");
+  var underBanner = document.getElementById("underBanner");
   // Add the 'moved' class to trigger the animation
+  banner.classList.remove("unmove");
   banner.classList.add("moved");
+  underBanner.classList.add("show");
   document.body.classList.add("scrolled");
   learning = true;
+}
+
+function unmove() {
+  var banner = document.getElementById("about");
+  // Add the 'moved' class to trigger the animation
+  banner.classList.remove("moved");
+  banner.classList.add("unmove");
+  document.body.classList.remove("scrolled");
+  learning = false;
 }
